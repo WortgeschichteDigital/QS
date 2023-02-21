3b88a05252a2358f96f51858d5ccb11c3ce3ed54
Nico Dorn <14349333+nicodorn@users.noreply.github.com>
2023-02-21 10:11:46 +0100

    [[fix]] beim Pullen wurden zwar die neusten Commits heruntergeladen, die Position von origin wurde im lokalen Repository aber nicht aufgefrischt, weswegen es so schien, als sei dieses Repository in der Geschichte voraus

--- js/win/git.js
+++ js/win/git.js
@@ -395,7 +395,7 @@ const git = {
         document.querySelector("#fun-git-pull").focus();
         return;
       }
-      const pull = await git.commandExec(`git pull https://${git.config.user}:${encodeURIComponent(configPass)}@${git.remote} ${branch}:${branch}`);
+      const pull = await git.commandExec(`git pull https://${git.config.user}:${encodeURIComponent(configPass)}@${git.remote} ${branch}:refs/remotes/origin/${branch}`);
       if (pull === false) {
         document.querySelector("#fun-git-pull").focus();
         return;
