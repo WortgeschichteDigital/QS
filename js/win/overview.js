a9cc2ec26dd4c613eb7b9ff1423aad66e14af207
Nico Dorn <14349333+nicodorn@users.noreply.github.com>
2024-06-09 18:12:19 +0200

    [[update]] Template für die Artikelübersicht auf zdl.org auf den neusten Stand gebracht

--- js/win/overview.js
+++ js/win/overview.js
@@ -53,7 +53,7 @@ const overview = {
 
     // fill in articles
     const content = overview.make(noNew);
-    const page = tt.replace('<div id="wgd-liste"></div>', `<div id="wgd-liste">\n${content}\n</div>`);
+    const page = tt.replace("{{RESULT}}", content);
 
     // return page if called via CLI
     if (cli) {
@@ -98,246 +98,103 @@ const overview = {
         continue;
       }
 
+      // publication year
+      const [ year ] = file.published.split("-");
+
+      // title
+      const lemmas = {
+        hl: [],
+        nl: [],
+      };
+      for (const [ k, v ] of Object.entries(lemmas)) {
+        for (const i of file[k + "Joined"]) {
+          v.push(shared.hidxPrint(i));
+        }
+      }
+      let title = lemmas.hl.join("&nbsp;· ");
+      if (lemmas.nl.length) {
+        title += ` (${lemmas.nl.join("&nbsp;· ")})`;
+      }
+
       // article link
-      const artLink = encodeURIComponent(shared.hidxClear(file.hl[0]));
+      const link = encodeURIComponent(shared.hidxClear(file.hl[0]));
 
-      // Hauptlemmata
-      for (const hl of file.hl) {
-        // create entry
-        const text = shared.hidxClear(hl);
-        const entry = {
-          letter: letter(hl),
-          id: file.id,
-          // "typ" has to be in German
-          typ: file.fa ? "Wortfeldartikel" : "Hauptlemma",
-          text,
-          link: (file.fa ? "Wortfeld-" : "") + artLink,
-          list: [],
-        };
-        entries.push(entry);
-
-        // fill lemma list if applicable
-        if (file.hlJoined.length > 1) {
-          // the main lemma is in an article with multiple main lemmas
-          // (as regards this, multiple spellings don't count)
-          entry.link = "";
-          entry.list.push({
-            text: shared.hidxClear(file.hlJoined, true).join("&nbsp;· "),
-            link: artLink,
-            nl: false,
-          });
-        }
-        if (file.nl.length) {
-          // the main lemma's article has sub lemmas
-          for (const nl of file.nlJoined) {
-            const [ lemma ] = nl.split("/");
-            entry.list.push({
-              text: shared.hidxClear(nl),
-              link: artLink + "#" + encodeURIComponent(file.nlTargets[lemma]),
-              nl: true,
-            });
-          }
-        } else if (file.fa) {
-          // this is a field article
-          for (const lemma of file.faLemmas) {
-            for (const link of file.links) {
-              if (link.lemma.spelling !== lemma) {
-                continue;
-              }
-              // detect lemma type and further spellings
-              const target = xml.data.files[link.lemma.file];
-              const nl = !!target.nlTargets[link.lemma.spelling];
-              const joined = nl ? target.nlJoined : target.hlJoined;
-              const reg = new RegExp(`(^|/)${shared.escapeRegExp(shared.hidxClear(lemma))}(/| \\(|$)`);
-              let text;
-              for (const i of joined) {
-                if (reg.test(i)) {
-                  text = shared.hidxClear(i);
-                  break;
-                }
-              }
-              if (!entry.list.some(i => i.text === text)) {
-                const artLink = encodeURIComponent(shared.hidxClear(target.hl[0]));
-                let link;
-                if (nl) {
-                  link = artLink + "#" + encodeURIComponent(target.nlTargets[lemma]);
-                } else {
-                  link = artLink;
-                }
-                entry.list.push({
-                  text,
-                  link,
-                  nl,
-                });
-              }
-              break;
-            }
-          }
-        }
-
-        // create reference
-        createReferences(entry, text);
-      }
-
-      // Nebenlemmata
-      for (const nl of file.nl) {
-        // create entry
-        const text = shared.hidxClear(nl);
-        const entry = {
-          letter: letter(nl),
-          id: file.id,
-          // "typ" has to be in German
-          typ: "",
-          text,
-          link: "",
-          list: [
-            {
-              text: shared.hidxClear(file.hlJoined.join("&nbsp;· "), true),
-              link: artLink + "#" + encodeURIComponent(file.nlTargets[nl]),
-              nl: false,
-            },
-          ],
-        };
-        entries.push(entry);
-
-        // create reference
-        createReferences(entry, text);
-      }
-    }
-
-    // create references
-    function createReferences (entry, entryText) {
-      // don't create reference because:
-      //   * the entry only consists of one (relevant) word
-      const text = entryText.replace(shared.artReg, "");
-      if (!/ /.test(text)) {
-        return;
-      }
-
-      // collect upper case words
-      const words = text.split(" ");
-      const upper = [];
-      for (const word of words) {
-        if (/^[A-ZÄÖÜ]/.test(word)) {
-          upper.push(word);
-        }
-      }
-
-      // don't create a reference because:
-      //   * all words are upper case
-      //     (but allow references for namelike expressions like "Friedliche Revolution")
-      //   * the first word is the only upper case word
-      const isAdjSub = /^[A-ZÄÖÜ][^\s]+(bar|lich|isch)e[mnrs]? [A-ZÄÖÜ][^\s]+$/.test(text);
-      if (upper.length === words.length && !isAdjSub ||
-          upper.length === 1 && words[0] === upper[0]) {
-        return;
-      }
-
-      // create reference(s)
-      for (let i = 0, len = upper.length; i < len; i++) {
-        // detect text
-        let ref;
-        if (upper[i] === words[0]) {
-          continue;
-        } else if (isAdjSub ||
-            upper.length === 1 && upper[0] === words[words.length - 1]) {
-          ref = upper[i] + ", " + entryText.replace(/ [^\s]+$/, "");
-        } else {
-          ref = upper[i] + "&nbsp;→ " + entryText;
-        }
-
-        // push new entry
-        const newEntry = structuredClone(entry);
-        newEntry.letter = letter(ref);
-        newEntry.text = ref;
-        if (!newEntry.link) {
-          newEntry.link = newEntry.list[0].link;
-        }
-        entries.push(newEntry);
-      }
+      // push entry
+      entries.push({
+        year: parseInt(year, 10),
+        date: file.published,
+        title,
+        link,
+        fa: file.fa,
+      });
     }
 
     // sort entries
     shared.sortModeForLemmas = true;
-    entries.sort((a, b) => shared.sort(a.text, b.text));
+    entries.sort((a, b) => {
+      // sort by year
+      if (a.year !== b.year) {
+        return b.year - a.year;
+      }
+
+      // sort by date
+      if (a.date !== b.date) {
+        const x = [ a.date, b.date ];
+        x.sort();
+        if (x[0] === a.date) {
+          return 1;
+        }
+        return -1;
+      }
+
+      // sort by title
+      return shared.sort(a.title, b.title);
+    });
     shared.sortModeForLemmas = false;
 
     // make and return HTML code
     const base = "[% base %]wb/wortgeschichten/";
-    let currentLetter = "";
     let html = "";
-    for (const i of entries) {
-      // print alpha block
-      if (currentLetter !== i.letter) {
-        currentLetter = i.letter;
-        html += `<div class="wgd-ab"><p>${currentLetter}</p></div>\n\n`;
-      }
-      // print entry
-      html += "<div";
-      for (const ds of [ "id", "typ" ]) {
-        if (i[ds]) {
-          html += ` data-${ds}="${i[ds]}"`;
-        }
-      }
-      html += ">\n<p>";
-      let text = i.text.replace(/\//, "/<wbr>");
-      if (!i.link) {
-        html += shy(text);
-      } else {
-        let ref = "";
-        if (/→/.test(text)) {
-          const refText = text.split("→ ");
-          ref = refText[0] + "→ ";
-          text = refText[1];
-        }
-        html += `${ref}<a href="${base}${i.link}">${shy(text)}</a>`;
-      }
-      html += "</p>\n";
-      if (i.list.length && !/→/.test(i.text)) {
-        html += "<ul>\n";
-        for (const li of i.list) {
-          const cl = li.nl ? ' class="wgd-nl"' : "";
-          const liText = li.text.replace(/\//, "/<wbr>");
-          html += `${" ".repeat(2)}<li${cl}><a href="${base}${li.link}">${shy(liText)}</a></li>\n`;
-        }
-        html += "</ul>\n";
-      }
-      html += "</div>\n\n";
-    }
-    return html.trim();
+    const years = {
+      last: 0,
+      count: 0,
+    };
 
-    // detect letter of the alphabet
-    function letter (lemma) {
-      const text = lemma.replace(shared.artReg, "");
-      const first = text.substring(0, 1);
-      if (/[0-9]/.test(first)) {
-        return "#";
-      } else if (/ä/i.test(first)) {
-        return "A";
-      } else if (/ö/i.test(first)) {
-        return "O";
-      } else if (/ü/i.test(first)) {
-        return "U";
-      }
-      return first.toUpperCase();
-    }
+    html = indent(2) + "<ul>";
+    for (const entry of entries) {
+      // add new section
+      // TODO for now, do not print the publication year (this is for political reasons only)
+      if (entry.year !== years.last) {
+        // if (html) {
+        //   html += indent(4) + "</ul>";
+        //   html += indent(2) + "</section>";
+        // }
 
-    // insert soft hypen(s)
-    // (this code is also used for zdl.org:
-    // static/wgd/start/art.mjs)
-    function shy (entryText) {
-      const rep = new Set([
-        [ /(?<=[a-z])bewegung/, "&shy;bewegung" ],
-        [ /(?<=[a-z])durchsetzungs(?=[a-z])/, "&shy;durchsetzungs&shy;" ],
-        [ /(?<=[a-z])theoretiker/, "&shy;theoretiker" ],
-        [ /(?<=[a-z])theorie/, "&shy;theorie" ],
-        [ /(?<=[a-z])verlierer/, "&shy;verlierer" ],
-      ]);
-      let text = entryText;
-      for (const [ k, v ] of rep) {
-        text = text.replace(k, v);
+        if (years.count === 2) {
+          break;
+        }
+        years.count++;
+        years.last = entry.year;
+
+        // html += indent(2) + "<section>";
+        // html += indent(4) + `<h3>${entry.year}</h3>`;
+        // html += indent(4) + "<ul>";
       }
-      return text;
+
+      // add entry
+      // const date = entry.date.match(/(?<year>[0-9]{4})-(?<month>[0-9]{2})-(?<date>[0-9]{2})/);
+      const wfa = entry.fa ? ' class="wgd-wfa"' : "";
+
+      // html += indent(6) + `<li><a href="${base + entry.link}" title="publiziert am ${date.groups.date.replace(/^0/, "")}. ${date.groups.month.replace(/^0/, "")}. ${date.groups.year}"${wfa}>${entry.title}</a></li>`;
+      html += indent(6) + `<li><a href="${base + entry.link}"${wfa}>${entry.title}</a></li>`;
+    }
+    html += indent(2) + "</ul>";
+
+    return html;
+
+    // return a new line with a proper indent
+    function indent (n) {
+      return "\n" + " ".repeat(n);
     }
   },
 };
